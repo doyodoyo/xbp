95fc740cbd422df4f5445644d56500ed014cc340
yamazaki@kanagawa-u.ac.jp <yamazaki@kanagawa-u.ac.jp>
2021-02-26 14:08:13 +0900

    docs: add comment

--- python/txtanalysis/main.py
+++ python/txtanalysis/main.py
@@ -18,8 +18,9 @@ docs = [nlp(s) for s in df['文書']]
 # 共起語関係取得
 df_word_count = pd.concat([utils.get_co_df(d) for d in docs]).reset_index(drop=True)
 
-# 記号 助詞　接続詞を除外
-# extract_pos = ['ADP', 'INTJ', 'PUNCT', 'SCONJ']
+# 除外する品詞の指定
+# ADJ: 形容詞, ADP: 設置詞, ADV: 副詞, AUX: 助動詞, CCONJ: 接続詞, DET: 限定詞, INTJ: 間投詞, NOUN: 名詞, NUM: 数詞,
+# PART: 助詞, PRON: 代名詞, PROPN: 固有名詞, PUNCT: 句読点, SCONJ: 連結詞, SYM: シンボル, VERB: 動詞, X: その他
 extract_pos = ['ADP', 'INTJ', 'PUNCT', 'SCONJ', 'PART', 'CCONJ', 'AUX', 'DET', 'SCONJ', 'VERB', 'X', 'SYM', 'NUM']
 
 df_ex_word_count = df_word_count[(~df_word_count['word1_pos'].isin(extract_pos)) \
